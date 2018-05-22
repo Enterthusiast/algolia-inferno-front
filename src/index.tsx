30496c82b330bc0df3bcd9fcab655f25359e51ba
Damien <damien@leclubdesextras.com>
2018-05-22 18:10:30 +0200

    First commit with an inferno boilerplate + typescript

---
+++ src/index.tsx
@@ -0,0 +1,53 @@
+// inferno module
+import {render} from 'inferno';
+
+// routing modules
+import { Router, Link, Route, IndexRoute } from 'inferno-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+// css
+import '../src/main.css';
+
+const browserHistory = createBrowserHistory();
+function App({ children }) {
+    return (
+		<div>
+			<h1>Algolia Inferno Test</h1>
+			<Link to="/test">Test</Link>
+            {children}
+		</div>
+    )
+}
+
+function NoMatch({ children, params }) {
+    return (
+		<div>no match</div>
+    )
+}
+
+function Home({ children }) {
+    return (
+		<div>home</div>
+    )
+}
+
+function Test({ children, params }) {
+
+    return (
+		<div>
+			<h2>test</h2>
+		</div>
+    )
+}
+
+const routes = (
+	<Router history={browserHistory}>
+		<Route component={App}>
+			<IndexRoute component={Home} />
+			<Route path="/test" component={Test} />
+			<Route path="*" component={NoMatch} />
+		</Route>
+	</Router>
+);
+
+render(routes, document.getElementById('app'));
---
+++ webpack.conf.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+	entry: "./src/index.tsx", // Point to main file
+	output: {
+        path: __dirname + '/public',
+		filename: 'bundle.js',
+		publicPath: 'http://localhost:8080/'
+	},
+	resolve: {
+		extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
+	},
+	performance: {
+		hints: false
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/, 						  // All ts and tsx files will be process by
+				loaders: [ 'babel-loader', 'ts-loader' ], // first babel-loader, then ts-loader
+				exclude: /node_modules/                   // ignore node_modules
+			}, {
+				test: /\.jsx?$/,                          // all js and jsx files will be processed by
+				loader: 'babel-loader',                   // babel-loader
+				exclude: /node_modules/                  // ignore node_modules
+			}, {
+				test: /\.css$/,
+				loaders: [ 'style-loader', 'css-loader' ],
+				exclude: /node_modules/
+			}
+		]
+	},
+	devServer: {
+		contentBase: './',
+		port: 8080,
+		noInfo: false,
+		hot: true,
+		inline: true,
+		proxy: {
+			'/': {
+				bypass: function (req, res, proxyOptions) {
+					return '/public/index.html';
+				}
+			}
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin(
+			{
+				template: "./src/index.html",
+				inject: "body"
+			}
+		),
+		new CleanWebpackPlugin(
+			["public"], {
+				verbose: true
+			}
+		),
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
