2aa1794ec09fbe256062cb375899723a87f5f039
Damien <damien@leclubdesextras.com>
2018-05-23 17:55:04 +0200

    fix wrong linking of bundle.js

--- webpack.conf.js
+++ webpack.conf.js
@@ -8,7 +8,7 @@ module.exports = {
 	output: {
         path: __dirname + '/public',
 		filename: 'bundle.js',
-		publicPath: 'http://localhost:8080/'
+		publicPath: ''
 	},
 	resolve: {
 		extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
