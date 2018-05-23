767be6b2c314eb4a5275fa074eb40de6991d36bd
Damien <damien@leclubdesextras.com>
2018-05-23 18:01:34 +0200

    new build in public folder

--- webpack.conf.js
+++ webpack.conf.js
@@ -8,7 +8,7 @@ module.exports = {
 	output: {
         path: __dirname + '/public',
 		filename: 'bundle.js',
-		publicPath: ''
+		publicPath: './'
 	},
 	resolve: {
 		extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
