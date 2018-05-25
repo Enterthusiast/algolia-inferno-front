76db547ce78e0fb976a765e101fc5f1ae8cba126
Damien <damien@leclubdesextras.com>
2018-05-25 12:42:32 +0200

    Add toggle faceting on mobile

--- src/component/faceting.tsx
+++ src/component/faceting.tsx
@@ -3,6 +3,14 @@ import { linkEvent, Component } from 'inferno';
 export default class Search extends Component<any, any> {
   constructor(props) {
     super(props);
+
+    // register on resize event
+    // so we can reset show/hide facet
+    window.onresize = (e) => {
+      if(e.target['visualViewport'].width > 600) {
+        this.resetVisibility();
+      }
+    }
   }
 
   handleClick(instance, event) {
@@ -31,7 +39,7 @@ export default class Search extends Component<any, any> {
     if(this.props.facets && this.props.facets.length > 0) {
       return this.props.facets.map((facet: any) => {
         return (
-          <div className="collection with-header">
+          <div id="faceting" className="collection with-header responsive-faceting faceting-small-hide">
             <div className="collection-header"><h6>{facet.name[0].toUpperCase() + facet.name.slice(1)}</h6></div>
             {this.dataList(facet)}
           </div>
@@ -44,9 +52,31 @@ export default class Search extends Component<any, any> {
     } 
   }
 
+  resetVisibility() {
+    const facetingElem = document.getElementById('faceting');
+    facetingElem.classList.remove('faceting-small-show');
+    facetingElem.classList.add('faceting-small-hide')
+  }
+
+  toggleVisibility(instance) {
+    const facetingElem = document.getElementById('faceting');
+    if(facetingElem.classList.contains('faceting-small-hide')) {
+      facetingElem.classList.remove('faceting-small-hide');
+      facetingElem.classList.add('faceting-small-show')
+    } else {
+      facetingElem.classList.remove('faceting-small-show');
+      facetingElem.classList.add('faceting-small-hide')
+    }
+  }
+
   render() {
     return (
       <div>
+        <div className="responsive-faceting-button center-align">
+          <a class="waves-effect waves-light btn" onClick={linkEvent(this, this.toggleVisibility)}>
+            Toggle Filter(s)
+          </a>
+        </div>
         {this.facetList()}
       </div>
     )
